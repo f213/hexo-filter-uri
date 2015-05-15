d5a20ac991c0d6684b360f26442fa59f359b58c0
Fedor Borshev <f@f213.in>
2015-05-16 01:15:20 +0300

    unicode bugfix

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var URI = require('uri-js');
 var blog_host = URI.parse(hexo.config.url)['host'];
 
 hexo.extend.filter.register('after_post_render', function(data){
-    $ = cheerio.load(data.content);
+    $ = cheerio.load(data.content, {
+        decodeEntities: false
+    });
 
     $('a').each(function(){
         var link_host = URI.parse($(this).attr('href'))['host']
